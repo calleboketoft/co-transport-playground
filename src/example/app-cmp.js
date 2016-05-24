fbf2cbaeae7508a53867dc779b427e167c218b79
Carl-Ola Boketoft <calleboketoft@gmail.com>
2016-05-24 13:21:17 +0200

    interval

--- src/example/app-cmp.js
+++ src/example/app-cmp.js
@@ -10,21 +10,33 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var socket_io_service_1 = require('../services/socket-io-service');
+var Rx_1 = require('rxjs/Rx');
 var AppCmp = (function () {
     function AppCmp() {
+        this.startInterval$ = new core_1.EventEmitter();
+        this.stopInterval$ = new core_1.EventEmitter();
         this.counter = 0;
         socket_io_service_1.socket.on('new message', function (msg) {
             console.log('message: ' + msg);
         });
+        var interval$ = Rx_1.Observable.interval(1000);
+        var intervalThatStops$ = interval$
+            .takeUntil(this.stopInterval$);
+        this.startInterval$
+            .switchMapTo(intervalThatStops$)
+            .do(function (x) {
+            socket_io_service_1.socket.emit('new message ', 'auto ' + x);
+        })
+            .subscribe(function (x) { return console.log(x); });
     }
-    AppCmp.prototype.sendMessage = function () {
-        socket_io_service_1.socket.emit('new message', 'testing ' + this.counter);
+    AppCmp.prototype.sendMessage = function (messageText) {
+        socket_io_service_1.socket.emit('new message', 'manual ' + this.counter);
         this.counter++;
     };
     AppCmp = __decorate([
         core_1.Component({
             selector: 'app',
-            template: "\n    <div class='container'>\n      <h1>Socket.io test</h1>\n      <button type='btn btn-primary' (click)='sendMessage()'>\n        Emit\n      </button>\n    </div>\n  "
+            template: "\n    <div class='container'>\n      <h1>Socket.io test</h1>\n      <button type='btn btn-primary'\n        (click)='sendMessage(\"manual click message\" + counter)'>\n        Send one manual message\n      </button>\n      <button type='btn btn-primary'\n        (click)='startInterval$.emit()'>\n        Start interval\n      </button>\n      <button type='btn btn-primary'\n        (click)='stopInterval$.emit()'>\n        Stop interval\n      </button>\n    </div>\n  "
         }), 
         __metadata('design:paramtypes', [])
     ], AppCmp);
--- src/example/app-cmp.ts
+++ src/example/app-cmp.ts
@@ -1,29 +1,52 @@
-import {Component} from '@angular/core'
+import {Component, EventEmitter} from '@angular/core'
 import {socket} from '../services/socket-io-service'
-
+import {Observable} from 'rxjs/Rx'
 
 @Component({
   selector: 'app',
   template: `
     <div class='container'>
       <h1>Socket.io test</h1>
-      <button type='btn btn-primary' (click)='sendMessage()'>
-        Emit
+      <button type='btn btn-primary'
+        (click)='sendMessage("manual click message" + counter)'>
+        Send one manual message
+      </button>
+      <button type='btn btn-primary'
+        (click)='startInterval$.emit()'>
+        Start interval
+      </button>
+      <button type='btn btn-primary'
+        (click)='stopInterval$.emit()'>
+        Stop interval
       </button>
     </div>
   `
 })
 export class AppCmp {
+  public startInterval$ = new EventEmitter();
+  public stopInterval$ = new EventEmitter();
+
   constructor () {
     socket.on('new message', (msg) => {
       console.log('message: ' + msg)
     })
+
+    const interval$ = Observable.interval(1000)
+
+    const intervalThatStops$ = interval$
+      .takeUntil(this.stopInterval$)
+
+    this.startInterval$
+      .switchMapTo(intervalThatStops$)
+      .do((x) => {
+        socket.emit('new message ', 'auto ' + x)
+      })
+      .subscribe((x) => console.log(x))
   }
 
   public counter = 0;
-
-  public sendMessage () {
-    socket.emit('new message', 'testing ' + this.counter)
+  public sendMessage (messageText) {
+    socket.emit('new message', 'manual ' + this.counter)
     this.counter++
   }
 }
